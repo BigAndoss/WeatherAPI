8cb21e1375ba74dc3c786b53d18c792f0889d553
Andoss <63303993+BigAndoss@users.noreply.github.com>
2025-01-05 18:59:39 +0100

    Prod Ready v0.1

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,22 @@ import { connectDB } from './config/db.js';
 import cityRoutes from './routes/city.route.js';
 import weatherRoutes from './routes/weather.route.js';
 
+const __dirname = path.resolve();
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ;
 // Middleware to parse JSON data
 app.use(express.json());
 
 app.use('/api/cities', cityRoutes)
 app.use('/api/weather', weatherRoutes)
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname,'frontend/dist')));
+  app.get('*',(req,res) => 
+    res.sendFile(path.resolve(__dirname,"frontend",'dist','index.html') ))
+}
 
 // Start the server
 app.listen(PORT, () => {
